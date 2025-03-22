a2f5197b0e6276b72717f492313e1c6c0bae2055
toobi-jpeg <rex.98@live.se>
2025-03-22 16:03:32 +0100

    link logic almost done + design updates.

--- scripts/script.js
+++ scripts/script.js
@@ -29,33 +29,43 @@ let fullDate =
   date.getUTCDate() + " " + month[date.getMonth() - 1] + " " + year;
 dateText.textContent = fullDate;
 // ..........................................................................
-// Snabblänkar data .........................................................
-
-let links = {
-  icon: "",
-  name: "",
-  url: "",
-};
-
-// ..........................................................................
 // Snabblänkar logik ........................................................
 const addLinkBtn = document.querySelector("#add-link-button");
 const linkInput = document.querySelector("#link-input");
+const linkInputDiv = document.querySelector("#add-link-div");
+const submitLinkBtn = document.querySelector("#add-link-icon-input");
 const linksContainer = document.querySelector("#links-container");
 
-function addLink(icon, name, url) {
+function addLink(urlTwo, name, url) {
   let linkDiv = document.createElement("div");
   linkDiv.classList.add("link-div");
+  linkDiv.addEventListener("click", () => {
+    urlLink = "https://";
+    url = urlLink.concat(url);
+    console.log("DET FUNKAR!" + url);
 
+    window.open(url, "_blank");
+  });
+
+  //icon variable länken skickades inte med genom addUrl funktionen så jag fick lägga denna logik här....
+  let getIcon = "https://www.google.com/s2/favicons?domain=";
+  icon = getIcon.concat(urlTwo);
+  //......
   let linkImg = document.createElement("img");
   linkImg.id = "favicon";
+  linkImg.src = icon;
+  linkImg.alt = name + " favicon";
 
   let linkName = document.createElement("p");
   linkName.id = "quick-link-span";
+  linkName.textContent = name;
 
   let deleteBtn = document.createElement("i");
   deleteBtn.id = "delete-link-button";
   deleteBtn.className = "fa-regular fa-circle-xmark";
+  deleteBtn.addEventListener("click", () => {
+    linkDiv.remove();
+  });
 
   linkDiv.appendChild(linkImg);
   linkDiv.appendChild(linkName);
@@ -64,27 +74,35 @@ function addLink(icon, name, url) {
 }
 
 addLinkBtn.addEventListener("click", () => {
-  linkInput.classList.add("show-input");
+  linkInputDiv.classList.add("show-input");
   linkInput.focus();
 });
 
 linkInput.addEventListener("keydown", (event) => {
   if (event.key === "Enter") {
     addUrl();
-    linkInput.classList.remove("show-input");
+    linkInputDiv.classList.remove("show-input");
   }
 });
 
+submitLinkBtn.addEventListener("click", () => {
+  addUrl();
+  linkInputDiv.classList.remove("show-input");
+});
+
 function addUrl() {
-  let url = linkInput.value;
-  console.log("Url inmatad:", url);
+  url = linkInput.value;
 
   let link = document.createElement("a");
   link.href = url;
 
+  //Går säkert att göra snyggare, denna del var jag tvungen att googla fram och tog ett par timmar att sätta ihop....
   let urlTwo = link.hostname.replace(/^www\./, "");
   let name = urlTwo.replace(/\.[a-z]+$/, "");
-  console.log(name);
+  name = name.replace(/^./, name[0].toUpperCase());
+  //......
+
+  addLink(url, name, urlTwo);
 
   linkInput.value = "";
 }
