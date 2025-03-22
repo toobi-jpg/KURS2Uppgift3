7923d4c0daf5ece4eb46ad5223bbbece51e17a19
toobi-jpeg <rex.98@live.se>
2025-03-22 04:31:37 +0100

    Some quicklinks logic added.

--- scripts/script.js
+++ scripts/script.js
@@ -29,4 +29,62 @@ let fullDate =
   date.getUTCDate() + " " + month[date.getMonth() - 1] + " " + year;
 dateText.textContent = fullDate;
 // ..........................................................................
-// Tid & datum logik ........................................................
+// Snabblänkar data .........................................................
+
+let links = {
+  icon: "",
+  name: "",
+  url: "",
+};
+
+// ..........................................................................
+// Snabblänkar logik ........................................................
+const addLinkBtn = document.querySelector("#add-link-button");
+const linkInput = document.querySelector("#link-input");
+const linksContainer = document.querySelector("#links-container");
+
+function addLink(icon, name, url) {
+  let linkDiv = document.createElement("div");
+  linkDiv.classList.add("link-div");
+
+  let linkImg = document.createElement("img");
+  linkImg.id = "favicon";
+
+  let linkName = document.createElement("p");
+  linkName.id = "quick-link-span";
+
+  let deleteBtn = document.createElement("i");
+  deleteBtn.id = "delete-link-button";
+  deleteBtn.className = "fa-regular fa-circle-xmark";
+
+  linkDiv.appendChild(linkImg);
+  linkDiv.appendChild(linkName);
+  linkDiv.appendChild(deleteBtn);
+  linksContainer.appendChild(linkDiv);
+}
+
+addLinkBtn.addEventListener("click", () => {
+  linkInput.classList.add("show-input");
+  linkInput.focus();
+});
+
+linkInput.addEventListener("keydown", (event) => {
+  if (event.key === "Enter") {
+    addUrl();
+    linkInput.classList.remove("show-input");
+  }
+});
+
+function addUrl() {
+  let url = linkInput.value;
+  console.log("Url inmatad:", url);
+
+  let link = document.createElement("a");
+  link.href = url;
+
+  let urlTwo = link.hostname.replace(/^www\./, "");
+  let name = urlTwo.replace(/\.[a-z]+$/, "");
+  console.log(name);
+
+  linkInput.value = "";
+}
